75e431a49d8d9cc62b2da7792e90f30cc650298e
toprakyasin0101@gmail.com <toprakyasin0101@gmail.com>
2022-10-12 01:03:21 +0300

    t

--- MyTrys3/Program.cs
+++ MyTrys3/Program.cs
@@ -11,69 +11,91 @@ namespace MyTrys3
             //categoryıd = 3 ==> Oyuncak
             //categoryıd = 4 ==> Oto Aksesuar
 
-            Product product1 = new Product();
-            product1.Id = 1;
-            product1.CategoryId = 1;
-            product1.Category = "Elektronik";
-            product1.ProductName = "Klavye";
-            product1.StockAmount = 1000;
-            product1.Price = 499.99;
+            string elektronik = "ELEKTRONİK KATEGORİSİ";
+            string mobilya = "MOBİLYA KATEGORİSİ";
+            string oyuncak = "OYUNCAK KATEGORİSİ";
+            string otoAksesuar = "OTO AKSESUAR KATEGORİSİ";
 
-            Product product2 = new Product();
-            product2.Id = 2;
-            product2.CategoryId = 2;
-            product2.Category = "Mobilya";
-            product2.ProductName = "Masa";
-            product2.StockAmount = 100;
-            product2.Price = 1499.99;
+            Product product1 = new Product()
+            {
+                Id = 1,
+                CategoryId = 1,
+                Category = "ELEKTRONİK",
+                ProductName = "Klavye",
+                StockAmount = 1000,
+                Price = 499.99           
+            };
 
-            Product product3 = new Product();
-            product3.Id = 3;
-            product3.CategoryId = 3;
-            product3.Category = "Oyuncak";
-            product3.ProductName = "Peluş Ayıcık";
-            product3.StockAmount = 350;
-            product3.Price = 99.90;
+            Product product2 = new Product()
+            {
+                Id = 2,
+                CategoryId = 2,
+                Category = "MOBİLYA",
+                ProductName = "Masa",
+                StockAmount = 100,
+                Price = 1499.99
+            };
+           
+            Product product3 = new Product() 
+            {
+                Id = 3,
+                CategoryId = 3,
+                Category = "OYUNCAK",
+                ProductName = "Peluş Ayıcık",
+                StockAmount = 350,
+                Price = 99.90
+            };
+          
+            Product product4 = new Product() 
+            {
+                Id = 4,
+                CategoryId = 4,
+                Category = "OTO AKSESUAR",
+                ProductName = "Lastik",
+                StockAmount = 60,
+                Price = 999.98
+            };
+           
+            Product product5 = new Product() 
+            {
+                Id = 5,
+                CategoryId = 3,
+                Category = "OYUNCAK",
+                ProductName = "Barbie Oyuncak",
+                StockAmount = 1250,
+                Price = 1999.99
+            };
+          
+            Product product6 = new Product() 
+            {
+                Id = 6,
+                CategoryId = 4,
+                Category = "OTO AKSESUAR",
+                ProductName = "Jant",
+                StockAmount = 360,
+                Price = 500.00
+            };
+            
+            Product product7 = new Product() 
+            {
+                Id = 7,
+                CategoryId = 2,
+                Category = "MOBİLYA",
+                ProductName = "Sandalye",
+                StockAmount = 150,
+                Price = 250.00
+            };
+           
+            Product product8 = new Product() 
+            {
+                Id = 8,
+                CategoryId = 1,
+                Category = "ELEKTRONİK",
+                ProductName = "Monitör",
+                StockAmount = 50,
+                Price = 2999.99
+            };
 
-            Product product4 = new Product();
-            product4.Id = 4;
-            product4.CategoryId = 4;
-            product4.Category = "Oto Aksesuar";
-            product4.ProductName = "Lastik";
-            product4.StockAmount = 60;
-            product4.Price = 999.88;
-
-            Product product5 = new Product();
-            product5.Id = 5;
-            product5.CategoryId = 3;
-            product5.Category = "Oyuncak";
-            product5.ProductName = "Barbie Oyuncak";
-            product5.StockAmount = 1250;
-            product5.Price = 199.99;
-
-            Product product6 = new Product();
-            product6.Id = 6;
-            product6.CategoryId = 4;
-            product6.Category = "Oto Aksesuar";       
-            product6.ProductName = "Jant";
-            product6.StockAmount = 360;
-            product6.Price = 500.00;
-
-            Product product7 = new Product();
-            product7.Id = 7;
-            product7.CategoryId = 2;
-            product7.Category = "Mobilya";
-            product7.ProductName = "Sandalye";
-            product7.StockAmount = 150;
-            product7.Price = 250.00;
-
-            Product product8 = new Product();
-            product8.Id = 8;
-            product8.CategoryId = 1;
-            product8.Category = "Elektronik";
-            product8.ProductName = "Monitör";
-            product8.StockAmount = 50;
-            product8.Price = 2999.99;
 
             ProductManager products = new ProductManager();
             products.Add(product1);
@@ -107,7 +129,7 @@ namespace MyTrys3
             Console.WriteLine("**************************************************************************");
             Product[] products1 = new Product[] {product1,product8 };
 
-            Console.WriteLine("ELEKTRONİK KATEGORİSİ");
+            Console.WriteLine(elektronik);
 
             foreach (var product in products1)
             {
@@ -117,7 +139,7 @@ namespace MyTrys3
             Console.WriteLine("**************************************************************************");
             Product[] products2 = new Product[] { product2, product7 };
 
-            Console.WriteLine("MOBİLYA KATEGORİSİ");
+            Console.WriteLine(mobilya);
 
             foreach (var product in products2)
             {
@@ -127,7 +149,7 @@ namespace MyTrys3
             Console.WriteLine("**************************************************************************");
             Product[] products3 = new Product[] { product3, product5 };
 
-            Console.WriteLine("OYUNCAK KATEGORİSİ");
+            Console.WriteLine(oyuncak);
 
             foreach (var product in products3)
             {
@@ -137,7 +159,7 @@ namespace MyTrys3
             Console.WriteLine("**************************************************************************");
             Product[] products4 = new Product[] { product4, product6 };
 
-            Console.WriteLine("OTO AKSESUAR KATEGORİSİ");
+            Console.WriteLine(otoAksesuar);
 
             foreach (var product in products4)
             {
